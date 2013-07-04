f844fe91134d916df9d6047c49fab0be9e17b255
Mateus Braga <mateus.a.braga@gmail.com>
2013-07-04 01:08:47 -0300

    Limpar loader.h

--- loader.h
+++ loader.h
@@ -1,14 +1,7 @@
-/* Estrutura do arquivo CLASS */
-
 #ifndef LOADER_H_
 #define LOADER_H_
 
 #include "structs.h"
-
-
-// todo tipo termina com _t: como em class_t
-// toda struct termina com _struct como em class_struct
-// olhar arquivo structs.h
 
 void loadClass(class_t* class);
 
