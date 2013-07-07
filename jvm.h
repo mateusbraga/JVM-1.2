a43d952e358ed4f84654e7dc2ed435cd1d1a583f
Mateus Braga <mateus.a.braga@gmail.com>
2013-07-07 15:23:00 -0300

    Documentando jvm.c

--- jvm.h
+++ jvm.h
@@ -8,7 +8,7 @@
 Utf8_info_t* string_to_utf8(char* a);
 int compare_utf8(Utf8_info_t* a, Utf8_info_t* b);
 u2 get_utf8_length_from_char(char* string);
-uint16_t scan_utf8_char_from_char(char* string, u2 *pos);
+u2 scan_utf8_char_from_char(char* string, u2 *pos);
 any_type_t* char_to_array_reference(char* string);
 any_type_t* utf8_to_array_reference(Utf8_info_t* utf8);
 // UTF8 STUFF - END
